065c360d5a81a7f2a34f60a36e2d8e0b5915c010
Patrick Kontschak <github@patrickkontschak.com>
2020-05-18 14:13:28 +0200

    use template literals for addRow
    Easier to maintain

--- src/functions.js
+++ src/functions.js
@@ -4,93 +4,42 @@ export function addRow(el) {
     // increase row number
     updateRow('add')
 
+    const rowContent = `
+        <div class="label" data-label="Leistung">
+            <input type="text" />
+        </div>
+        <div class="label" data-label="Objekt">
+            <select>
+                <option></option>
+                <option></option>
+            </select>
+        </div>
+        <div class="label" data-label="Preis netto">
+            <div class="currency-input">
+                <input type="text" id="row-${row}-netto" placeholder="0.00" />
+            </div>
+        </div>
+        <div class="label" data-label="Mwst">
+            <select id="row-${row}-mwst">
+                <option value="0">0%</option>
+                <option value="7">7%</option>
+                <option value="19">19%</option>
+            </select>
+        </div>
+        <div class="label" data-label="Preis brutto">
+            <div class="currency-input">
+                <input type="text" id="row-${row}-brutto" placeholder="0.00" />
+            </div>
+        </div>
+        <div class="label" data-label="Entfernen">
+            <i class="far fa-trash-alt remove-row"></i>
+        </div>
+    `
+
     // row div#row
     const newRow = document.createElement('div')
           newRow.className = 'row'
-
-    // leistung div.label > input
-    const leistungLabel = document.createElement('div')
-          leistungLabel.className = 'label'
-          leistungLabel.setAttribute('data-label', 'Leistung')
-    const leistungInput = document.createElement('input')
-          leistungInput.type = 'text'
-    leistungLabel.appendChild(leistungInput)
-
-    // objekt div.label > select > option
-    const objectLabel = document.createElement('div')
-          objectLabel.className = 'label'
-          objectLabel.setAttribute('data-label', 'Objekt')
-    const objectSelect = document.createElement('select')
-    for (let index = 0; index < 2; index++) {
-        const option = document.createElement('option')
-        objectSelect.appendChild(option)
-    }
-    objectLabel.appendChild(objectSelect)
-
-    // netto div.label > div.currency-input > input#row-N-netto
-    const nettoLabel = document.createElement('div')
-          nettoLabel.className = 'label'
-          nettoLabel.setAttribute('data-label', 'Preis netto')
-    const nettoWrapper = document.createElement('div')
-          nettoWrapper.className = 'currency-input'
-    nettoLabel.appendChild(nettoWrapper)
-    const nettoInput = document.createElement('input')
-          nettoInput.id = `row-${row}-netto`
-          nettoInput.type = 'text'
-          nettoInput.placeholder = '0.00'
-    nettoWrapper.appendChild(nettoInput)
-
-    // mwst div.label > select#row-N-mwst > option
-    const mwstLabel = document.createElement('div')
-          mwstLabel.className = 'label'
-          mwstLabel.setAttribute('data-label', 'Mwst')
-    const mwstSelect = document.createElement('select')
-          mwstSelect.id = `row-${row}-mwst`
-    const mwst0 = document.createElement('option')
-          mwst0.value = 0
-    const mwst0text = document.createTextNode('0%')
-    mwst0.appendChild(mwst0text)
-    mwstSelect.appendChild(mwst0)
-    const mwst7 = document.createElement('option')
-          mwst7.value = 7
-    const mwst7text = document.createTextNode('7%')
-    mwst7.appendChild(mwst7text)
-    mwstSelect.appendChild(mwst7)
-    const mwst19 = document.createElement('option')
-          mwst19.value = 19
-    const mwst19text = document.createTextNode('19%')
-    mwst19.appendChild(mwst19text)
-    mwstSelect.appendChild(mwst19)
-    mwstLabel.appendChild(mwstSelect)
-
-    // brutto div.label > div.currency-input > input#row-N-brutto
-    const bruttoLabel = document.createElement('div')
-          bruttoLabel.className = 'label'
-          bruttoLabel.setAttribute('data-label', 'Preis brutto')
-    const bruttoWrapper = document.createElement('div')
-          bruttoWrapper.className = 'currency-input'
-    const bruttoInput = document.createElement('input')
-          bruttoInput.id = `row-${row}-brutto`
-          bruttoInput.type = 'text'
-          bruttoInput.placeholder = '0.00'
-    bruttoWrapper.appendChild(bruttoInput)
-    bruttoLabel.appendChild(bruttoWrapper)
-
-    // trash div.label > i.remove-row
-    const trashLabel = document.createElement('div')
-          trashLabel.className = 'label'
-          trashLabel.setAttribute('data-label', 'Entfernen')
-    const trashButton = document.createElement('i')
-          trashButton.className = 'far fa-trash-alt remove-row'
-    trashLabel.appendChild(trashButton)
-
-    // merge it all together
-    newRow.appendChild(leistungLabel)
-    newRow.appendChild(objectLabel)
-    newRow.appendChild(nettoLabel)
-    newRow.appendChild(mwstLabel)
-    newRow.appendChild(bruttoLabel)
-    newRow.appendChild(trashLabel)
+          newRow.innerHTML = rowContent
 
     // insert
     document.getElementById('calculator').insertBefore(newRow, el)
